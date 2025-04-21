6b84c85390f01741ba30086c1afaaee6d25f6555
Rujal <rujalshrsth@gmail.com>
2025-04-21 15:04:30 +0545

    add new meals screens

---
+++ lib/screens/meals.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Uh nothinig here',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Try adding some meals',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) {
+          return ListTile(title: Text(meals[index].title));
+        },
+      );
+    }
+
+    return Scaffold(appBar: AppBar(title: Text(title)), body: content);
+  }
+}
