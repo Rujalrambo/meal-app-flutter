766eb77726609f48dc1a60865599f2b1a79c0e3f
Rujal <rujalshrsth@gmail.com>
2025-04-23 13:50:00 +0545

    adding msg for adding and removing data

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -14,13 +14,26 @@ class _TabsScreensState extends State<TabsScreens> {
   int _selectedPageIndex = 0;
   final List<Meal> _favoriteMeals = [];
 
+  void _showInfoMessage(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message), duration: const Duration(seconds: 2)),
+    );
+  }
+
   void _toggleMealFavouriteStatus(Meal meal) {
     final isExisting = _favoriteMeals.contains(meal);
 
     if (isExisting) {
-      _favoriteMeals.remove(meal);
+      setState(() {
+        _favoriteMeals.remove(meal);
+        _showInfoMessage('Meal removed from favorites!');
+      });
     } else {
-      _favoriteMeals.add(meal);
+      setState(() {
+        _favoriteMeals.add(meal);
+        _showInfoMessage('Meal added to favorites!');
+      });
     }
   }
 
@@ -39,7 +52,7 @@ class _TabsScreensState extends State<TabsScreens> {
 
     if (_selectedPageIndex == 1) {
       activePage = MealsScreen(
-        meals: [],
+        meals: _favoriteMeals,
         onToggleFavourite: _toggleMealFavouriteStatus,
       );
       activePageTitle = 'Your Favourites';
